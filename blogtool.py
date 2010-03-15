6775e37249d626aa92bce15cb29b5dd76299febd
Gerry LaMontagne <gjlama94@gmail.com>
2010-03-15 07:46:34 -0400

    Adds header fields to blank post file

--- blogtool.py
+++ blogtool.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
-import blogapi
-import btconfigparser
-import html2md
-
 from optparse import OptionParser
 from tempfile import NamedTemporaryFile
+from StringIO import StringIO
+
+import xml.etree.cElementTree as etree
+import unicodedata as ud
+
+import blogapi
+
+import btconfigparser
 
 import time
 import datetime
@@ -16,34 +20,20 @@ import types
 import subprocess
 
 try:
-    from lxml import etree
-except ImportError:
-    print """
-ATTENTION:
-You need to install lxml to take full advantage of blogtool capabilities.
-Without it, downloading posts is disallowed.  If all you need it to publish
-posts to a blog, then this should be fine.
-
-Unfortunately, this annoying warning message will pop-up everytime you run
-blogtool.
-"""
-    raw_input('Press <ENTER> to continue...')
-    import xml.etree.cElementTree as etree
-
-try:
     import markdown
-    MARKDOWN_PRESENT = True
 except ImportError:
     print """
-ATTENTION:
-In order to publish posts with blogtool, python-markdown is required.  Without
-it, all blogtool is capable of is some basic blog interaction for listing posts
-or listing/ modifying categories.
-
-Also, this annoying message will be displayed everytime you run blogtool.
+You need to install python-markdown before you can use blogtool.
 """
-    raw_input('Press <ENTER> to continue...')
-    MARKDOWN_PRESENT = False
+    sys.exit()
+
+#try:
+#    import BeautifulSoup 
+#except ImportError:
+#    print """
+#You need to install BeautifulSoup before you can use blogtool.
+#"""
+#    sys.exit()
 
 #################################################################################
 #
@@ -458,39 +448,29 @@ class blogtool():
 
     ############################################################################ 
     def doOptionGetPost(self):
-        if not html2md.LXML_PRESENT:
-            print "Option not supported without python-lxml library."
-            return
-
         # retrieve a post from blog
+        print "Retrieving post %s from blog %s..." % (self.opts.get_postid, 
+                                                      self.bc.name)
+
         post = self.blogproxy.getPost(self.opts.get_postid)
-# the following lines are for debug purposes
 #        for k,v in post.iteritems():
 #            print "%s : %s" % (k, v)
-#        print repr(post['description'])
-#        print repr(post['mt_text_more'])
+        print repr(post['description'])
+        print repr(post['mt_text_more'])
 
-        text = html2md.convert(post['description'])
+        md = html2md()
+        text = md.convert(post['description'])
         if post['mt_text_more']:
             text += "<!--more-->\n\n"
-            text += html2md.convert(post['mt_text_more'])
+            mdmore = html2md()
+            text += mdmore.convert(post['mt_text_more'])
 
-        print 'BLOG: %s\nPOSTID: %s\nTITLE: %s\nCATEGORIES: %s' % (
-               self.bc.name, 
-               self.opts.get_postid, 
-               post['title'], 
-               ', '.join(post['categories']))
-        if post['mt_keywords']:
-            print 'TAGS: %s' % ', '.join(post['mt_keywords'])
+        print text
 
-        print '\n' + text
+        sys.exit()
 
     ############################################################################ 
     def procPost(self):
-        if not MARKDOWN_PRESENT:
-            print "Unable to publish post without python-markdown.  Sorry..."
-            sys.exit()
-
         # when we run the text through markdown, it will preserve the linefeeds of 
         # the original text.  This is a problem when publishing because the blog
         # software turns the linefeeds within the 'p' tags to 'br' tags which
@@ -750,6 +730,310 @@ class blogtool():
         else:
             f.close()
 
+#################################################################################
+class html2md:
+    def __init__(self):
+
+        self.out = ''
+        self._tags = []  # holds nested tags- not the same as the last
+                         # processed tag since tags are removed from the list
+                         # when the end of the tag is reached
+        self._opened_tags = []
+        self._last_processed = None  # last tag processed
+        self.bq = ''
+        self.ol_tag = 1
+        self.reflinks = 0
+        self.links = []
+        self.start_handlers = dict(
+                                   [
+                                    ('a', self.a_start),
+                                    ('p', self.p_start),
+                                    ('blockquote', self.bq_start),
+                                    ('code', self.code_start),
+                                    ('em', self.em_start),
+                                    ('strong', self.strong_start),
+                                    ('li', self.li_start),
+                                    ('ul', self.ul_start),
+                                    ('ol', self.ol_start)
+                                   ]
+                                  )
+        self.end_handlers = dict (
+                                  [
+                                   ('a', self.a_end),
+                                   ('p', self.p_end),
+                                   ('blockquote', self.bq_end),
+                                   ('li', self.li_end),
+                                   ('ul', self.ul_end),
+                                   ('ol', self.ol_end)
+                                  ] 
+                                 )
+
+    def convert(self, html):
+        uhtml = ud.normalize('NFKD', unicode(html))
+
+        events = ('start', 'end')
+        for action, e in etree.iterparse(StringIO("<post>%s</post>" % uhtml),
+                                         events = events):
+            if action == 'start':
+                self._tag_enter(e.tag)
+                if e.tag in self.start_handlers.keys():
+                    self.start_handlers[e.tag](e)
+                else:
+                    self.generic_start_handler(e)
+            
+            if action == 'end':
+                if e.tag in self.end_handlers.keys():
+                    self.end_handlers[e.tag](e)
+                else:
+                    self.generic_end_handler(e)
+
+                self._tag_exit(e.tag)
+
+        return self.out
+ 
+    def _outAppend(self, s):
+        # if we're in a blockquote, then prepend the '> '
+        if 'blockquote' in self._tags:
+            self.out += self.bq
+
+        # make sure that there is text- if the tag starts with, say, a link
+        # then text will be None
+        if s:
+            self.out += s
+
+    def generic_start_handler(self, e):
+        if e.text:
+            self.out += (e.text.lstrip()).rstrip('\n')
+
+    def generic_end_handler(self, e):
+        pass
+
+    def p_start(self, p):
+        # if the p-tag occurs within a list, then remove the 
+        # trailing '\n' because it will get added on by the p-tag
+        # processing
+        if self._last_tag() == 'li':
+            if self._opened_tags[-2] == 'p':
+                self.out += '    '
+            else:
+                self.out = self.out[:-1]
+
+        self._outAppend(p.text)
+
+    def p_end(self, p):
+        if len(self.links) != 0:
+            self._outAppend("\n\n%s" % self.reflinkText())
+        
+        self.out += '\n'
+        self._outAppend('\n')
+
+    def bq_start(self, bqe):
+        self.bq += '> '
+
+    def bq_end(self, bqe):
+        self.bq = self.bq[2:]
+        self.out = self.out[:-4]
+        
+        self.out += '\n'
+        if not self.bq:
+            self.out += '\n'
+
+    def a_start(self, a):
+        pass
+
+    def a_end(self, a):
+        # first, put the text together
+        self._outAppend("[%s][%s]" % (a.text, self.reflinks))
+        self._outAppend(self._checktail(a))
+
+        # now, save off reflink and link itself
+        if 'title' not in a.attrib.keys():
+            a.set('title', None)
+
+        self.links.append((self.reflinks, a.attrib['href'], a.attrib['title']))
+        self.reflinks += 1
+
+    def code_start(self, code):
+        last_tag = self._last_tag()
+        if last_tag == 'p':
+            self._outAppend(self.convert_inline(code, '`'))
+        elif last_tag == 'pre':
+            for line in code.text.split('\n'):
+                self._outAppend('    %s\n' % line)
+
+    def em_start(self, em):
+        # if the text ends with a space, then add it after the *
+        self._outAppend(self.convert_inline(em, '*'))
+
+    def strong_start(self, strong):
+        self._outAppend(self.convert_inline(strong, '**'))
+
+    def li_start(self, li):
+        last_tag = self._last_tag()
+        if last_tag == 'ul':
+            self._outAppend('* %s' % li.text)
+        elif last_tag == 'ol':
+            self._outAppend('%s.  %s' % (self.ol_tag, li.text))
+            self.ol_tag += 1
+
+    def li_end(self, li):
+        # we don't need to advance the line if we are in a list and 
+        # processing a p-tag- the p-tag processing will add linefeeds
+        if (self._opened_tags[-2] not in [ 'ul', 'ol' ] and
+            self._last_processed == 'p'):
+            pass
+        else:
+            # advance line for next list item
+            self.out += '\n'
+
+    def ol_start(self, ol):
+        pass
+
+    def ol_end(self, ol):
+        if ol.tail and not ol.tail.isspace():
+            self._outAppend(ol.tail)
+        if self._opened_tags[-1] != 'p':
+            self._outAppend('\n')
+        self.ol_tag = 1
+
+    def ul_start(self, ul):
+        pass
+
+    def ul_end(self, ul):
+        if ul.tail and not ul.tail.isspace():
+            self._outAppend(ul.tail)
+        self._outAppend('\n')
+
+    def convert_inline(self, e, c):
+        addspace = False
+        if e.text.endswith(' '):
+            addspace = True
+
+        text = "%s%s%s" % (c, e.text.rstrip(), c)
+        if addspace:
+            text += ' '
+
+        text += self._checktail(e)
+
+        return text
+
+    def _checktail(self, element):
+        if element.tail and not element.tail.isspace():
+            return element.tail
+        else:
+            return ''
+
+    def reflinkText(self):
+        text = ''
+        for ref in self.links:
+            text += "[%s]: %s" % (ref[0], ref[1])
+            if ref[2] != None:
+                text += ''' "%s"''' % (ref[2])
+            text += '\n'
+
+        del self.links[:]
+
+        return text.rstrip()
+
+    def _tag_enter(self, t):
+        self._tags.append(t)
+        self._opened_tags.append(t)
+
+    def _tag_exit(self, t):
+        if self._current_tag() != t:
+            print "Stack Error: %s versus current %s" % (self._current_tag, t)
+            print self._tags
+            sys.exit()
+        else:
+            self._last_processed = self._tags.pop()  # remove current tag
+
+    def _current_tag(self):
+        ntags = len(self._tags)
+        if ntags > 0:
+            return self._tags[ntags - 1]
+        else:
+            return None
+
+    def _last_tag(self):
+        ntags = len(self._tags)
+        if ntags > 1:
+            return self._tags[ntags - 2]
+        else:
+            return None
+
+    def _last_opened(self):
+        return self._opened_tags[-2]
+
+#################################################################################
+## entry function for processing post text- image links and what not are
+## dealt with here
+##
+#def procPost(blog, blogname,  posttext):
+#
+#    # helper function for comprehension below
+#    def skiptag(tag):
+#        return (isinstance(tag, BeautifulSoup.Comment) or
+#                tag.parent.name == 'pre' or
+#                tag.parent.name == 'code')
+#
+#    # when we run the text through markdown, it will preserve the linefeeds of 
+#    # the original text.  This is a problem when publishing because the blog
+#    # software turns the linefeeds within the 'p' tags to 'br' tags which
+#    # defeats the formatting powers of HTML.  So we'll remove all of the
+#    # linefeeds in the 'p' tags.  Towards this end, we'll use Beautiful Soup
+#    # because it streamlines what would otherwise be a tedious process
+#    soup = BeautifulSoup.BeautifulSoup(markdown.markdown(posttext))
+#
+#    # remove extraneous newlines from the NavigableStrings (text)
+#    # for better or worse, I'll leave extra space (i.e. multiple consecutive
+#    # space characters) alone, for now.  Browser's get rid of the space so it 
+#    # doesn't seem to hurt anyone if I leave it in place
+#    # don't do this for 'pre' tags
+#    souptext = soup.findAll(text = True) 
+#    [ t.replaceWith(t.replace('\n', ' ')) for t in souptext if not skiptag(t) ]
+#
+#    # now deal with all the 'img' tags
+#    for img in soup.findAll('img'):
+#        # first, make sure this looks like a valid file
+#        imgf = img['src']
+#        if imgf.find("http://") == -1:
+#            if os.path.isfile(imgf) != 1:
+#                # try anchoring file to the user's home directory       
+#                imgf = os.path.expanduser('~') + '/' + imgf
+#                if os.path.isfile(imgf) != 1:
+#                    error("Image file not found: %s\n" % img['src'])
+#        else:
+#            # this is a link so don't proceed any further, move on to the next
+#            continue
+#
+#        # run it up the flagpole...
+#        print "Attempting to upload '%s'..." % imgf
+#        res = blog.upload(blogname, imgf)
+#        if res == None:
+#            print "Upload failed, proceeding...\n"
+#            continue
+#        print "Done"
+#
+#        # replace the image file name in the 'img' tag with the url and also
+#        # add the 'alt' attribute, assuming it wasn't provided
+#        # check for an 'res' attribute and append this to the filename while
+#        # removing it from the attribute list
+#        img['src'] = res['url']
+#        if not img.has_key('alt'):
+#            img['alt'] = res['file']
+#
+#        if img.has_key('res'):
+#            res_str = '-' + img['res'] + '.'
+#            img['src'] = re.sub("\.(\w*)$", r'%s\1' % res_str, img['src'])
+#            
+#            # the 'res' attr is bogus- I've added it so that I can specify the
+#            # appropriate resolution file I want in the url.  
+#            # remove it from the final post
+#            del(img['res'])
+#
+#    return soup.renderContents()
+#
+
 ################################################################################
 # 
 # Checks for and processes a config file for blogtool 
@@ -902,6 +1186,7 @@ def main():
     # now process the postfile, if any
     bt.doFilelist()
 
+    print 'Done.'
     sys.exit()
 
 ################################################################################
